4c84129344c40ead15bac49793f7e052b8b0e4dd
majamiljic <majamiljic2@gmail.com>
2018-09-20 14:25:08 +0200

    Angular Routing Basics

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,13 @@
+import { ContactComponent } from './../contact/contact.component';
+import { AboutComponent } from './../about/about.component';
+import { MenuComponent } from './../menu/menu.component';
+import { HomeComponent } from './../home/home.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'}
+]
